b57a315d7563f60a963b1ec3502f3cb10855b883
David Wallace <david.wallace@goodeggs.com>
2019-09-09 11:15:32 -0400

    add rollbar logging

--- setup.py
+++ setup.py
@@ -40,7 +40,8 @@ setup(
     },
     install_requires=[
         'requests==2.22.0',
-        'singer-python==5.7.0'
+        'singer-python==5.7.0',
+        'rollbar==0.14.7'
     ],
     python_requires='>=3.6',
     entry_points={
