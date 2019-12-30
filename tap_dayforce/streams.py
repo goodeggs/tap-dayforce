4a6381235a24a8268ff026f01f53e272ce779852
David Wallace <david.wallace@goodeggs.com>
2019-12-30 16:17:20 -0500

    update whitelist

--- tap_dayforce/streams.py
+++ tap_dayforce/streams.py
@@ -132,10 +132,11 @@ class EmployeesStream(DayforceStream):
 
     def whitelist_sensitive_info(self, data: Dict) -> Dict:
         for collection in WHITELISTED_COLLECTIONS:
-            for i, item in enumerate(data.get(collection, {}).get("Items", [{}])):
-                if item.get("PayPolicy") is None or item.get("PayPolicy").get("XRefCode") not in WHITELISTED_PAY_POLICY_CODES:
-                    for field in WHITELISTED_FIELDS:
-                        data[collection]["Items"][i].pop(field, None)
+            if data.get(collection, {}).get("Items") is not None:
+                for i, item in enumerate(data.get(collection, {}).get("Items")):
+                    if item.get("PayPolicy") is None or item.get("PayPolicy").get("XRefCode") not in WHITELISTED_PAY_POLICY_CODES:
+                        for field in WHITELISTED_FIELDS:
+                            data[collection]["Items"][i].pop(field, None)
 
         return data
 
