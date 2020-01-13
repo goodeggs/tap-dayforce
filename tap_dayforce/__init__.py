144635782b2487d9e0d3306e0440e13a3b5acf9c
David Wallace <david.wallace@goodeggs.com>
2020-01-13 15:36:39 -0500

    remove runtime error

--- tap_dayforce/__init__.py
+++ tap_dayforce/__init__.py
@@ -74,7 +74,6 @@ def sync(args):
 
 def main_impl():
     args = parse_args(required_config_keys={"username", "password", "client_namespace", "start_date"})
-    raise RuntimeError
     if args.discover:
         discover(args, select_all=args.select_all)
     elif not args.catalog:
